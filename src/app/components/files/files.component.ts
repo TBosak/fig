74c13cacdd64e97f9b5f0f55063f350028b39e57
Tim B <41713011+TBosak@users.noreply.github.com>
2024-02-01 16:02:15 -0600

    wip

--- src/app/components/files/files.component.ts
+++ src/app/components/files/files.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Signal, WritableSignal, signal } from '@angular/core';
+import { text } from 'cheerio/lib/api/manipulation';
+import { toObservable } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-files',
@@ -6,7 +8,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./files.component.scss']
 })
 export class FilesComponent implements OnInit {
-  selectedOpt: string = 'clipboard';
+  selectedOpt: WritableSignal<string> = signal('clipboard');
+  fileLinks: string[] = [];
   options=[
     {name: 'Clipboard', value: 'clipboard'},
     {name: 'File', value: 'file'},
@@ -16,6 +19,32 @@ export class FilesComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    toObservable(this.selectedOpt).subscribe((value) => {
+      switch(value){
+        case('clipboard'): this.getUrlsFromClipboard().then(urls => this.fileLinks = urls); break;
+      }
+    });
+  }
+
+  async updateSelectedOpt(event: Event) {
+    const target = event.target as HTMLSelectElement;
+    this.selectedOpt.set(target.value);
+  }
+
+  async getUrlsFromClipboard() {
+    try {
+      return await navigator.clipboard.readText().then(text => {
+        const urls = this.extractFileLinksFromText(text)
+        return urls || [];
+      });
+    } catch (err) {
+      console.error('Failed to read from clipboard:', err);
+      return Promise.resolve([]);
+    }
+  }
+
+  extractFileLinksFromText(text:string) {
+    return text.match(/\bhttps?:\/\/\S+\.(pdf|zip|tar|gz|docx|xlsx|pptx|mp3|mp4|jpg|jpeg|png|gif|csv)\b/gi) || [];
   }
 
 }
